4683bc31e45397776fc3ebaa734498e6c9adfa2c
Shawn Wilder <shawnkwilder@gmail.com>
2023-02-05 14:43:12 -0500

    Added a filter for hobbies

--- src/modules/program/program.component.tsx
+++ src/modules/program/program.component.tsx
@@ -1,14 +1,27 @@
 import dayjs from 'dayjs';
+import { MouseEventHandler, useState } from 'react';
 import { Resident } from '../resident';
 
 export function Program({ programs }: any) {
-    const renderProrgramCells = ({ id, name, location, start, end, hobbies, attendance } : any) => (
+    const [hobbyFilter, setHobbyFilter] = useState<string>('');
+
+    const renderProrgramCells = ({  name, location, start, end, hobbies, attendance } : any) => (
         <>
             <td>{name}</td>
             <td>{location}</td>
             <td>{dayjs(start).format('DD/MM/YYYY')}</td>
             <td>{dayjs(end).format('DD/MM/YYYY')}</td>
-            <td>{hobbies.sort().join(', ')}</td>
+            <td>{
+                hobbies.sort().map(
+                    (hobby:any, index: number) => 
+                        (<button
+                            value={hobby}
+                            onClick={(event: React.MouseEvent<HTMLButtonElement>) => setHobbyFilter(event.target instanceof HTMLButtonElement ? event.target.value : '')}  
+                            key={index}>
+                                {hobby}
+                        </button>)
+                    )}
+            </td>
             <td>{
                 attendance
                     .filter((attendee: any) => (attendee.status !== "Declined" ))
@@ -17,8 +30,10 @@ export function Program({ programs }: any) {
         </>
     );
 
+    const filteredPrograms = hobbyFilter ? programs.filter((program: any) => program.hobbies.includes(hobbyFilter)) : programs;
+
     return (
-    <>
+    <>  {hobbyFilter ? <button onClick={() => setHobbyFilter('')} >Clear Filter</button> : null}
         <table>
         <thead>
             <tr>
@@ -28,12 +43,11 @@ export function Program({ programs }: any) {
                 <th>End</th>
                 <th>Hobbies</th>
                 <th>Attendance</th>
-                
             </tr>
         </thead>
 
         <tbody>
-            {programs.map((program: any) => (
+            {filteredPrograms.map((program: any) => (
                 <tr key={program.id}>{renderProrgramCells(program)}</tr>
             ))}
         </tbody>
